9f923b5467a43c38c42279d60b8c0ea5d827ddda
Aditya Mahendrakar <a.mahendrakar@yahoo.com>
2020-08-11 08:08:58 -0700

    regen  proto files with newer protobuf

--- proto/sign.pb.go
+++ proto/sign.pb.go
@@ -610,11 +610,11 @@ var fileDescriptor_3feb3e12a3dc7fb1 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // SigningClient is the client API for Signing service.
 //
@@ -650,10 +650,10 @@ type SigningClient interface {
 }
 
 type signingClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewSigningClient(cc grpc.ClientConnInterface) SigningClient {
+func NewSigningClient(cc *grpc.ClientConn) SigningClient {
 	return &signingClient{cc}
 }
 
